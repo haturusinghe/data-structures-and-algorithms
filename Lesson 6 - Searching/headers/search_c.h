5182901f1393746672c81a58c0c7e01a506bf403
Shanilka Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2022-03-12 09:26:50 +0530

    added binary search

--- Lesson 6 - Searching/headers/search_c.h
+++ Lesson 6 - Searching/headers/search_c.h
@@ -20,3 +20,46 @@ int SequentialSearch(List list,KeyType target){
     }
 }
 
+/* RecBinary2: recursive recognizing equality version of binary search.
+Pre: The contiguous list list has been created, and bottom and top are indices of
+entries in the list.
+Post: If an entry in list between bottom and top, inclusive, has key equal to target,
+then the function returns the location of one such entry (success). Otherwise,
+the function returns - 1 (failure). */
+int RecBinary2(List list, ListEntry target, int bottom, int top) {
+  int middle = -1;
+  if (bottom <= top) {
+    middle = (top + bottom) / 2;
+    if ((target < list.items[middle])) /* Reduce to the bottom half. */
+      middle = RecBinary2(list, target, bottom, middle - 1);
+    else if ((target > list.items[middle])) /* Reduce to the top half. */
+      middle = RecBinary2(list, target, middle + 1, top);
+  }
+  return middle;
+}
+
+int RecBinary2Search(List list, ListEntry target) {
+  return RecBinary2(list, target, 0, listSize(&list)-1);
+}
+
+/* Binary2Search: recognizing equality version of binary search.
+Pre: The contiguous list list has been created.
+Post: If an entry in list has key equal to target, then the function returns the location
+of one such entry (success). Otherwise the function returns - 1 (failure). */
+
+int Binary2Search(List list, ListEntry target) {
+  int bottom, middle, top;
+  top = list.count - 1; /* Initialize bounds to encompass entire list. */
+  bottom = 0;
+  while (top >= bottom) {
+    /* Check terminating condition. */
+    middle = (top + bottom) / 2;
+    if ((target == list.items[middle]))
+      return middle;
+    else if ((target < list.items[middle]))
+      top = middle - 1; /* Reduce to the bottom half of the list. */
+    else
+      bottom = middle + 1; /* Reduce to the top half of the list. */
+  }
+  return -1;
+}
