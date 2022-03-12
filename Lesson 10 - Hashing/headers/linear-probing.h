cc568a5e2f02855f623b79e5d3d865fc78737db0
Shanilka Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2022-03-12 15:40:37 +0530

    linear probing sample code

---
+++ Lesson 10 - Hashing/headers/linear-probing.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+/*
+int hashFunction(int key);
+
+
+int insertKey  = 0;
+int hIndex = hashFunction(insertKey);
+bool found = false;
+
+while(HT[hIndex].key != emptyKey && !found){
+    if(HT[hIndex].key == insertKey){
+        found = true;
+    }else{
+        hIndex = (hIndex + 1) % HTSize;
+    }
+}
+
+if(found){
+    Error("Duplicate Key");
+}else{
+    HT[hIndex] = newItem
+}
+*/
