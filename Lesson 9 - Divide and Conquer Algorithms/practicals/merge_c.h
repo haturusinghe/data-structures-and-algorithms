19f92d82b5fd8c2e027de560d8f39456043a92cd
Shanilka Haturusinghe <haturusi-cs18015@stu.kln.ac.lk>
2022-03-15 20:14:46 +0530

    merge sort for array

---
+++ Lesson 9 - Divide and Conquer Algorithms/practicals/merge_c.h
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge(int* arr,int start, int end);
+
+void mergeSort(int* arr, int start, int end){
+
+    if(start >= end){
+        return;
+    }else{
+        int mid = (start + end) / 2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid+1, end);
+        merge(arr, start, end);
+    }
+    
+}
+
+void merge(int* a, int start, int end){
+    
+    int i = start;
+    int mid = (start + end) / 2;
+    int j = mid + 1;
+
+    int k = 0;
+    int temp[end - start + 1];
+
+    while(i <= mid && j <= end){
+        if(a[i] < a[j]){
+            temp[k++] = a[i++];
+        }else{
+            temp[k++] = a[j++];
+        }
+    }
+
+    while(i <=mid ){
+        temp[k++] = a[i++];
+    }
+
+    while(j <= end){
+        temp[k++] = a[j++];
+    }
+
+    for(int p = 0; p < k; p++){
+        a[start + p] = temp[p];
+    }
+
+}
